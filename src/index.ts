e2d568d7ddb19ad52a058ca17e6476629d486e6f
Narayan Maity <narayanmaitysp1997@gmail.com>
2025-06-20 18:06:14 +0530

    adding more parameter to test the response

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,14 @@ async function main() {
 	const response = await openai.chat.completions.create({
 		model: 'gpt-4o',
 		messages: [{ role: 'user', content: 'Say something cosmic cool' }],
-		frequency_penalty: 1.5,
+		frequency_penalty: 1.5, // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
+		n: 2, // gives two responses just need to remove the choices[0] to choices
+		max_completion_tokens: 500, //An upper bound for the number of tokens that can be generated for a completion, including visible output tokens and reasoning tokens.
+
+		// there are more parameters but need to test as the times goes with the application design
 	});
 
-	console.log(response.choices[0].message.content);
+	console.log(response.choices);
 }
 
 function encodePrompt() {
